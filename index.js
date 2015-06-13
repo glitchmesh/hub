ca0cd78701cebbb57cbe005592eba8053f9d42e7
imakewebthings <caleb@imakewebthings.com>
2015-06-13 14:57:03 -0700

    Split senders and receivers, send messages to receivers

--- index.js
+++ index.js
@@ -11,10 +11,33 @@ var server = new WebSocket.Server({
 server.on('connection', handleConnection)
 
 function handleConnection (connection) {
+  console.log('new connection')
   connection.on('message', handleMessage)
 }
 
 function handleMessage (message) {
-  console.log(message)
+  var json = JSON.parse(message)
+
+  if (json.channel) {
+    registerChannel(this, json.channel)
+  } else if (this.sender) {
+    console.log('new message received')
+    console.log('broadcasting')
+    server.clients.forEach(function (socket) {
+      if (socket.receiver) {
+        socket.send(message)
+      }
+    })
+  }
 }
 
+function registerChannel(socket, channel) {
+  if (channel.indexOf('sender') > -1) {
+    socket.sender = true
+    console.log('sender registered')
+  }
+  if (channel.indexOf('receiver') > -1) {
+    socket.receiver = true
+    console.log('receiver registered')
+  }
+}
